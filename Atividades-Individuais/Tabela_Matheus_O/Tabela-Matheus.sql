1af893a922d22f8927f5ab632a0b02e6ffc2719b
SirMath200223 <matheus.osilva@sptech.school>
2024-03-17 14:44:53 -0300

    INSERT e SELECT da minha tabela individual

--- Atividades-Individuais/Tabela_Matheus_O/Tabela-Matheus.sql
+++ Atividades-Individuais/Tabela_Matheus_O/Tabela-Matheus.sql
@@ -1,7 +1,7 @@
 CREATE TABLE cliente (
 idCliente int primary key auto_increment,
 nome varchar (50) NOT NULL,
-senha varchar (30) NOT NULL,
+senha varchar (20) NOT NULL,
 cpf char (11) NOT NULL DEFAULT 'Possui rne',
 rne char (8) NOT NULL DEFAULT 'Possui cpf',
 celular char (9) NOT NULL,
@@ -19,7 +19,22 @@ cnpjEmpresa char (14) NOT NULL
 CREATE TABLE estufa (
 idAlface int primary key auto_increment,
 tipo varchar (15) NOT NULL CONSTRAINT chkTipo CHECK (tipo IN('Crespa', 'Americana', 'Ambas')),
-tamanho varchar (30),
+tamanho varchar (20),
 temperatura decimal (4,2),
 humidade int
 );
+
+INSERT INTO cliente (nome, senha, cpf, rne, celular, email) VALUES
+	('Paulo', 'UD*(CASMnicas', '53987562414', '876110867', '897560251', 'Paulo.s@gmail.com');
+    
+SELECT * FROM cliente;
+
+INSERT INTO empresa (nomeEmpresa, enderecoEmpresa, numeroSede, cnpjEmpresa) VALUES
+	('Agro', 'Rua Inventada, Loucura, SP', '57', '46131350478203');
+    
+SELECT * FROM empresa;
+
+INSERT INTO estufa (tipo, tamanho, temperatura, humidade) VALUES
+	('Crespa', '50 Km²', '23', '80');
+    
+SELECT * FROM estufa;
